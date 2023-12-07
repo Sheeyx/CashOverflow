30764db02a4ee9bad9793a9fe96afd5f8286db3e
Sheeyx <sheeyx00@gmail.com>
2023-12-07 18:24:26 +0900

    DATA: Add storage Broker

--- Cashoverflow/Brokers/Storages/StorageBroker.Locations.cs
+++ Cashoverflow/Brokers/Storages/StorageBroker.Locations.cs
@@ -6,4 +6,19 @@ namespace Cashoverflow.Brokers.Storages;
 public partial class StorageBroker
 {
     public DbSet<Location> Locations { get; set; }
+
+    public async ValueTask<Location> InsertLocationAsync(Location location) =>
+        await InsertAsync(location);
+
+    public IQueryable<Location> SelectAllLocations() =>
+        SelectAll<Location>();
+
+    public async ValueTask<Location> SelectLocationByIdAsync(Guid id) =>
+        await SelectAsync<Location>(id);
+
+    public async ValueTask<Location> UpdateLocationAsync(Location location) =>
+        await UpdateAsync(location);
+
+    public async ValueTask<Location> DeleteLocationAsync(Location location) =>
+        await DeleteAsync<Location>(location);
 }
